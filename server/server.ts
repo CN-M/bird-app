d6a58bd9eb56480ff2d5bdd4d662a2cf1edb0e1d
C.N. <cn.mbhalati@gmail.com>
2024-07-22 23:19:03 +0200

    fix(server.ts): increase rate limit to 250 requests per minute to handle higher traffic and improve performance

--- server/server.ts
+++ server/server.ts
@@ -34,7 +34,7 @@ const allowedOrigins = [
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  limit: 100, // Limit each IP to  requests per `window` (here, per 15 minutes).
+  limit: 250, // Limit each IP to  requests per `window` (here, per 1 minute).
   standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false,
 });
